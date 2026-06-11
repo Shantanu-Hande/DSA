525e51641ffee5b3f9ae9a90cd196e54c826b2db
Shantanu-Hande <handeshantanu0@gmail.com>
2026-06-11 23:48:19 +0530

    Updated Array Leetcode

--- Array/Array Leetcode/Q_283.java
+++ Array/Array Leetcode/Q_283.java
@@ -1,5 +1,9 @@
-class Solution {
-    public void moveZeroes(int[] nums) {
+// 283. Move Zeroes
+
+public class Q_283{
+    
+    //Brute Force Approach
+    public void moveZeroes(int[] nums) { 
         for(int i=0 ; i<nums.length ; i++){
             for(int j=0 ; j<nums.length - i - 1 ; j++){
                 if(nums[j]==0){
@@ -7,6 +11,19 @@ class Solution {
                     nums[j] = nums[j+1];
                     nums[j+1] = temp;
                 }
+            }
+        }
+    }
+
+    //Optimized Solution ==> Two Poointer
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+        for(int right=0 ; right<nums.length ; right++){
+            if(nums[right] != 0){
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
             }
         }
     }
