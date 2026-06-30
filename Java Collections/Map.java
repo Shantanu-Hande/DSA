db9059f4f3645035380f12e58ab001b63322c1c4
Shantanu-Hande <handeshantanu0@gmail.com>
2026-06-30 22:27:41 +0530

    Updated Java Collections

---
+++ Java Collections/Map.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Map{
+
+    // HashMap --> Store key in random order
+    // Key, value
+    // public static void main(String args[]){
+    //     HashMap<Integer,String> mp = new HashMap<>();
+    //     mp.put(2,"Shantanu");
+    //     mp.put(1,"Dnyanuuu");
+    //     mp.put(3,"Yash");
+    //     System.out.println(mp.size());
+    //     mp.remove(3);
+    //     System.out.println(mp.get(2));
+    //     System.out.println(mp);
+    // }
+
+    //TreeMap --> Stores key in sorted order
+    public static void main(STring args[]){
+        TreeMap<In,String> 
+    }
+}
